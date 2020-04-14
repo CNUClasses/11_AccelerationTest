40ada3b7cf083a378ac0c25809da9221e416ed6f
keith <keith.perkins@cnu.edu>
2020-04-14 11:38:22 -0400

    update gradle

--- app/src/main/java/com/example/accelerationtest/AccelerationTest.java
+++ app/src/main/java/com/example/accelerationtest/AccelerationTest.java
@@ -111,12 +111,13 @@ public class AccelerationTest extends Activity implements SensorEventListener {
 
     @Override
     protected void onResume() {
-	super.onResume();
-	// register this class as a listener for the orientation and
-	// accelerometer sensors
-	sensorManager.registerListener(this, 
-		sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 
-		SensorManager.SENSOR_DELAY_NORMAL);
+		super.onResume();
+
+		// register this class as a listener for the orientation and
+		// accelerometer sensors
+		sensorManager.registerListener(this,
+			sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+			SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
